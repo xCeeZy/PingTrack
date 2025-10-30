393a01ddaabab97303b44f6122fd92fb62b3ce4e
rusak <rusak@DESKTOP-RK3RGT9>
2025-10-30 20:43:41 +0300

    Добавьте файлы проекта.

---
+++ PingTrack/View/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PingTrack.View.Windows
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordBox.Password.Trim();
+
+            if (login == "admin" && password == "1234")
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ PingTrack/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using PingTrack.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PingTrack.View.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new DashboardPage());
+        }
+
+        private void DashboardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new DashboardPage());
+        }
+
+        private void PlayersBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Игроки' пока не создана.");
+        }
+
+        private void GroupsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Группы' пока не создана.");
+        }
+
+        private void TrainingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Занятия' пока не создана.");
+        }
+
+        private void JournalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Журнал' пока не создана.");
+        }
+
+        private void ReportsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Страница 'Отчёты' пока не создана.");
+        }
+    }
+}
